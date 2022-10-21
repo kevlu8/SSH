d2c51d04d0d5f2c47e666e02f8f0a2ca11d23e0e
wdotmathree <wdotmathree@gmail.com>
2022-10-21 16:21:40 -0600

    Make AES padding compliant with the SSH RFC

--- aes.c
+++ aes.c
@@ -1,13 +1,5 @@
 #include "aes.h"
 
-int pad(char **padded, const char *buf, size_t len) {
-	size_t padlen = 17 - (len + 1 % 16);
-	*padded = malloc(len + padlen);
-	memcpy(*padded, buf, len);
-	memset(*padded + len, padlen, padlen);
-	return len + padlen;
-}
-
 aes_ctx *aes_init(const uint8_t *key, const uint64_t nonce, const uint64_t iv) {
 	aes_ctx *ctx = malloc(sizeof(aes_ctx));
 	if (ctx == NULL) {
@@ -20,10 +12,8 @@ aes_ctx *aes_init(const uint8_t *key, const uint64_t nonce, const uint64_t iv) {
 }
 
 void aes_encrypt(aes_ctx *ctx, char **dst, const char *src, size_t len) {
-	char *padded;
-	int paddedlen = pad(&padded, src, len);
-	*dst = malloc(paddedlen);
-	for (int i = 0; i < paddedlen; i += 16) {
+	*dst = malloc(len);
+	for (size_t i = 0; i < len; i += 16) {
 		uint8_t block[16];
 		memcpy(block, &ctx->nonce, 8);
 		memcpy(block + 8, &ctx->ctr, 8);
@@ -33,23 +23,6 @@ void aes_encrypt(aes_ctx *ctx, char **dst, const char *src, size_t len) {
 			(*dst)[i + j] = src[i + j] ^ block[j];
 		}
 	}
-	free(padded);
 }
 
-int aes_decrypt(aes_ctx *ctx, char **dst, const char *src, size_t len) {
-	char *padded = malloc(len);
-	for (int i = 0; i < len; i += 16) {
-		uint8_t block[16];
-		memcpy(block, &ctx->nonce, 8);
-		memcpy(block + 8, &ctx->ctr, 8);
-		ctx->ctr++;
-		aes_encrypt_block(block, ctx->key);
-		for (int j = 0; j < 16; j++) {
-			padded[i + j] = src[i + j] ^ block[j];
-		}
-	}
-	int padlen = padded[len - 1];
-	*dst = malloc(len - padlen);
-	memcpy(*dst, padded, len - padlen);
-	free(padded);
-}
+inline void aes_decrypt(aes_ctx *ctx, char **dst, const char *src, size_t len) { aes_encrypt(ctx, dst, src, len); }
